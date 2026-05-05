3a8c2795291dad4850dd3d56dca04d59b1b5dba8
jhonManuelGil <jhonmanuelg6@gmail.com>
2026-05-05 01:01:12 -0300

    Uploading: class variables, system function

--- python/intraduucion.py
+++
@@ -1,3 +0,0 @@
-
-x = 'Hola mundo'
-print(x)
---
+++ python/script.py
@@ -0,0 +1,60 @@
+# ============= clase 00 ==========================
+
+# esto es uun comentario
+
+# nuestro hola mundo en python
+# print("Hola Mundo") # las comillas pueden ser simples o dobles
+# print('Hola Mundo') 
+# esto es una variable
+
+"""
+esto es otro comentario en varia líneas
+"""
+'''
+esto es otro comentario en varias líneas
+
+'''
+
+# types de datos
+
+# print(type("hola mundo")) # esto es un string o cadena de texto
+# print(type(5)) # esto es un entero
+# print(type(5.0)) # esto es un número decimal
+# print(type(True)) # esto es un booleano
+# print(type(5 + 5j)) # esto es un número complejo
+
+
+
+# ============= clase 001 variables ==========================
+
+
+MMystring = "esto es una variable de tipo string"
+print(MMystring)
+
+mn_ystring = 2 #"esto es una variable de tipo string correta por combenación "
+print(mn_ystring)
+
+my_string = 8  #"esto es una variable de tipo string"
+
+print(my_string)
+
+# tipo de argumentos
+
+print(type(MMystring), type(mn_ystring), type(my_string)) # esto es una función de tipo print con varios argumentos
+
+# print(type(print(MMystring, mn_ystring, my_string))) # esto en MMystring es una función de tipo print con varios argumentos dentro de otra función de tipo print
+
+print(type(MMystring)) 
+
+
+print("este es el valor de MMystring: ", MMystring) #
+
+# ====================  fusncion del sistema ===========================
+
+print(len(MMystring)) # la función de tipo len nos devuelve la cantidad de caracteres que tiene una cadena de texto
+
+# variable en una solo línea
+my_string_d, my_int_d, my_float_o = "hola mundo ", 5, 5.0
+print(my_string_d, my_int_d, my_float_o) # esto es una función de tipo print con varios argumentos
+
+
