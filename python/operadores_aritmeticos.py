d5b59cf06a3172a8ec37668a63d60c07a0503e27
jhonManuelGil <jhonmanuelg6@gmail.com>
2026-05-18 00:54:12 -0300

    Uploading update: DIO course arithmetic operators Python

---
+++ python/operadores_aritmeticos.py
@@ -0,0 +1,34 @@
+product_1 = 10
+prudoct_2= 20
+# suma
+
+print(product_1 + prudoct_2) # esto es una función de tipo print devoldiendo la suma de 30
+
+# resta
+
+print(product_1 - prudoct_2) # esto es una función de tipo print devoldiendo la resta de -10
+
+# multiplicación
+print(product_1 * prudoct_2) # esto es una función de tipo print devoldiendo la multiplicación de 200
+
+# división
+print(product_1 / prudoct_2) # esto es una función de tipo print devoldiendo la división de 0.5
+
+# división entera
+
+print(product_1 // prudoct_2) # esto es una función de tipo print devoldiendo la división entera de 0
+
+# módulo
+print(product_1 % prudoct_2) # esto es una función de tipo print devoldiendo el módulo de 10
+
+# potencia
+
+print(product_1 ** prudoct_2) # esto es una función de tipo print devoldiendo la potencia de 10 elevado a 20
+
+x = 10 + 5 - 8 / 5
+print(x) # esto es una función de tipo print devoldiendo el resultado de la operación aritmética de 14.4
+
+x = 10 + (5 - 8) / 5 # entre paréntesis se resuelve primero
+print(x) # esto es una función de tipo print devoldiendo el resultado de la operación aritmética de 9.4
+
+
