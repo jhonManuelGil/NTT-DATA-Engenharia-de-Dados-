9939bc27c8e56128dfb84c6e41cd3afa02e536d0
jhonManuelGil <jhonmanuelg6@gmail.com>
2026-06-08 00:51:47 -0300

    Uploading update: DIO course arithmetic operators Python

---
+++ python/Variáveis em Python/variaveis_python.py
@@ -0,0 +1,19 @@
+print("hola mundo")
+
+text = "esto es una variable"
+
+print(text)
+
+'''
+variable tem letras número y undercore (_) 
+# Ao criaer una variable, no puede comenzar 
+# con un número, pero puede contener números 
+# después de la primera letra o el guión bajo. 
+# Las variables también pueden contener letras 
+# mayúsculas y minúsculas, pero no pueden 
+# contener espacios. En su lugar, se puede 
+# usar un guión bajo para separar las palabras 
+# en una variable.
+
+'''
+
